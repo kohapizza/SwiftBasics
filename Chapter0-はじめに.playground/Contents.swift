418bbe7b49f61386d56221c86b4e023010dc6227
Koharu Saeki <koharu0929alexandros@gmail.com>
2024-05-11 20:55:29 +0900

    Basics

---
+++ Chapter0-はじめに.playground/Contents.swift
@@ -0,0 +1,13 @@
+import Foundation
+// これは、コメントです。メモなど好きなことを書くことができます。
+// import FoundationはXcodeであればimport UIKitでも同じ意味になります。
+//print("Hello Swift!")
+
+// 0.2 0.1で書いたprint関数をコメントアウトして実行されないことを確認してください。
+
+/* これは、コメントです。メモなど好きなことを書くことができます。
+import FoundationはXcodeであればimport UIKitでも同じ意味になります。 */
+
+// print("実行されない")
+
+
---
+++ Chapter9-繰り返し.playground/Contents.swift
@@ -0,0 +1,19 @@
+// 9.1 for文を使って配列coursesに格納されている値をコンソールに出力してください
+var courses = ["iPhone", "Android", "WebD", "WebS", "Movie"]
+
+for i in 0..<5{
+    print(courses[i])
+}
+
+for i in courses{
+    print(i)
+}
+
+// 9.2 レンジとfor文を使って1から10の値をコンソールに出力してください
+
+
+for j in 1..<11{
+    print(j)
+}
+
+
